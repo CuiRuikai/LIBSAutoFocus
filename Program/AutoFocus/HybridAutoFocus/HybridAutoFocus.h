f3791c9be08de5f5bc80e0b56ab3d71d7a499156
cuiruikai <trevorcui@163.com>
2019-03-11 23:00:12 +0800

    create AutoFoucs solution

---
+++ Program/AutoFocus/HybridAutoFocus/HybridAutoFocus.h
@@ -0,0 +1,32 @@
+
+// HybridAutoFocus.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CHybridAutoFocusApp: 
+// 有关此类的实现，请参阅 HybridAutoFocus.cpp
+//
+
+class CHybridAutoFocusApp : public CWinApp
+{
+public:
+	CHybridAutoFocusApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CHybridAutoFocusApp theApp;
